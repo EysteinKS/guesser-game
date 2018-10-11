5fd58ca20b3e65b746e5b56c88fe3b363044712b
drmabuse19 <40247440+drmabuse19@users.noreply.github.com>
2018-10-11 23:47:41 +0200

    CSS love
    litt css her og der

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import CreateWordContainer from "../components/CreateWordContainer"
-
+import "../css/Create.css"
 class Create extends Component {
     constructor(props){
         super(props)

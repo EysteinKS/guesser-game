9a58b58f6e8835f6cf3e674cc495b0cc0e1779ce
Eystein <eystein.kolsto@gmail.com>
2018-10-13 14:33:53 +0200

    Join Session button and Key input

--- src/components/CreateSessionContainer.jsx
+++ src/components/CreateSessionContainer.jsx
@@ -4,16 +4,39 @@ import { session } from "../firebase/index"
 class CreateSessionContainer extends Component {
     constructor(props){
         super(props)
+
+        this.state = {
+            sessionKey: ""
+        }
+
     }
+
+    handleChange = (event) =>
+        this.setState({ [event.target.name]: event.target.value })
+
 
     doCreateSession = () => {
         session.createSession()
+    }
+
+    doJoinSession = ( sessionkey ) => {
+        session.joinSession( sessionkey )
     }
 
     render(){
         return(
             <section>
                 <button onClick={this.doCreateSession}>Create Session</button>
+                <form onSubmit={this.doJoinSession}>
+                    <input 
+                        name="sessionKey"
+                        type="text"
+                        value={this.state.sessionKey}
+                        onChange={this.handleChange}
+                        placeholder="Session Key">
+                    </input>
+                    <input type="submit" value="Join"></input>
+                </form>
             </section>
         )
     }

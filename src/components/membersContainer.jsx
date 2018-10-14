3a5461f8b3aab3e89559704687f4f4360fd8ad82
drmabuse19 <40247440+drmabuse19@users.noreply.github.com>
2018-10-14 03:31:20 +0200

    s

--- src/components/membersContainer.jsx
+++ src/components/membersContainer.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { Component } from "react"
+import { SessionStore, UserStore } from "../store/Store"
 
-const MembersContainer = () =>
-    <div>
-        <p>Members...</p>
-    </div>
+class SessionContainer extends Component {
+    constructor(props){        
+        super(props)
+        SessionStore.addListener(this.onChange)
+        UserStore.addListener(this.onChange)
+    }
 
-export default MembersContainer;
+    componentWillUnmount() {
+        SessionStore.removeListener(this.onChange)
+        UserStore.removeListener(this.onChange)
+    }
+    onChange = () => {
+        this.forceUpdate()
+    }
+
+    render(){
+        return(
+            <section>
+                <p>Session started: {SessionStore["currentTime"]}</p>
+                <p>{SessionStore["activePlayers"]} players</p>
+            </section>
+        )
+    }
+
+}
+
+export default SessionContainer
